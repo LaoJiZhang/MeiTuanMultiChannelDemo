634d7b99757be46d119f70442b56e453818ea3a2
gmj <guomaojian@kaochong.com>
2017-05-04 15:15:17 +0800

    [需求] [美团V2自动化打包实现] 实现美团V2+Groovy脚本实现项目多渠道打包方案

---
+++ MultiChannelBuildTool.groovy
@@ -0,0 +1,32 @@
+def startTime = System.nanoTime()
+def channelFileName = "Channel.txt"
+
+/**
+ * @desc 遍历build目录文件夹下即将需要打包的Apk文件
+ */
+def originPath = new File("").getAbsolutePath() + File.separator
+def apkPath = "app" + File.separator + "build" + File.separator + "outputs" + File.separator + "apk" + File.separator
+File[] originApks = new File(originPath + apkPath).listFiles(new FilenameFilter() {
+    @Override
+    boolean accept(File file, String name) {
+//        return name.endsWith(".apk")
+        return name.contains("release") && name.endsWith(".apk")
+    }
+})
+
+def targetPath = originPath + apkPath + "app_channel_target_apk" + File.separator
+def targetDirFile = new File(targetPath)
+if (!targetDirFile.exists()) {
+    targetDirFile.mkdirs()
+}
+
+originApks.each { file ->
+    new File(originPath + "app" + File.separator + channelFileName).eachLine { channel ->
+        def newApkPath = targetPath + file.getName().replace(".apk", "-" + channel + ".apk")
+        new File(newApkPath).delete()
+        def singleCmd = "java -jar walle-cli-all.jar put -c " + channel + " " + file.getAbsolutePath() + " " + newApkPath
+        Process process = singleCmd.execute()
+        print "${process.text}"
+    }
+}
+println System.nanoTime() - startTime
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MeiTuanMultiChannelDemo
+利用美团新一代V2打包方案实现项目多渠道打包
+
+使用Groovy脚本自动生成渠道包，
+
+使用步骤：
+
+* 在AndroidStudio 利用插件生成原始release包
+* 在Terminal控制台执行 ./MultiChannel.sh 命令即可
+
+###使用注意事项：
+复制以下文件放置在您项目对应位置即可：（需注意需要您本地存在groovy运行环境即可）
+
+ * app/Channel.txt
+ * MultiChannel.sh
+ * MultiChannelBuildTool.groovy
+ * walle-cli-all.jar
